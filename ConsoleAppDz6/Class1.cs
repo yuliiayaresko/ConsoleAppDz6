5feb2022d091b095322ba85a514b9436ea30f662
yulia <yuliia_yaresko@knu.ua>
2024-12-04 19:01:13 +0200

    Add project files.

---
+++ ConsoleAppDz6/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleAppDz6
+{
+    public class PublishingHouse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+    }
+
+    public class Pidruchnuk
+    {
+        public int PublishingHouseId { get; set; }
+        public string Title { get; set; }
+        public PublishingHouse PublishingHouse { get; set; }
+    }
+
+
+
+
+
+}
+
---
+++ ConsoleAppDz6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using ConsoleAppDz6;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Pidruchnuk> books = new List<Pidruchnuk>();
+        const string filePath = "C:\\Users\\yulia\\source\\repos\\ConsoleAppDz6\\ConsoleAppDz6\\j.json";
+
+       
+            try
+            {
+                
+                string jsonContent = File.ReadAllText(filePath);
+
+               
+            books = JsonSerializer.Deserialize<List<Pidruchnuk>>(jsonContent);
+
+            foreach (var pidruchnuk in books)
+            {
+                 Console.WriteLine($"Title: {pidruchnuk.Title}");
+                    Console.WriteLine($"Publishing House ID: {pidruchnuk.PublishingHouseId}");
+                    Console.WriteLine($"Publishing House Name: {pidruchnuk.PublishingHouse.Name}");
+                    Console.WriteLine($"Address: {pidruchnuk.PublishingHouse.Adress}");
+                    Console.WriteLine();
+            }
+
+        }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Невідома помилка: {ex.Message}");
+            }
+
+
+        
+} }
